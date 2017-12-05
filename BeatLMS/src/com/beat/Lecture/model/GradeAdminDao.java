85ffc0261dfcbb81c74be269ed0a5d649477f37b
Cho <mincoln419@naver.com>
2017-12-05 12:33:57 +0900

    학생 성적 보기 dao 입니다. 입력은 admin에서 하고, 리스트 보기는 listdao에서 하겠습니다 아직 admin 쪽은 업뎃 안함..

--- BeatLMS/src/com/beat/Lecture/model/GradeAdminDao.java
+++ BeatLMS/src/com/beat/Lecture/model/GradeAdminDao.java
@@ -1,7 +1,23 @@
 package com.beat.Lecture.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.beat.util.LMSDao;
 
 public class GradeAdminDao extends LMSDao{
 
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	
+	GradeAdminDao(){
+		conn=super.conn;
+	}
+	
+	
+	
 }
